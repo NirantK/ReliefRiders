b8ade4cfc8f727c44f8780cec5b1695075958e8d
Nirant <NirantK@users.noreply.github.com>
2021-05-15 15:22:20 +0530

    black it

--- closestrider.py
+++ closestrider.py
@@ -6,6 +6,7 @@ from haversine import haversine
 
 st.title("Find Closest Relief Rider")
 
+
 @st.cache
 def load_data(
     url="https://docs.google.com/spreadsheets/d/e/2PACX-1vR17ZMSIIDqLKh9YZC2poBM7ts-sP8Psntdg--zo8F5Tb2TfMzZ9AawaY4_x2rVP4nNZgI5ULFNZtpl/pub?gid=2123468107&single=true&output=csv",
@@ -33,15 +34,21 @@ def load_data(
     # st.text(df.dtypes)
     return df
 
+
 # st.dataframe(df)
-help_input = st.text_input(label="Request Location", value="13.064058531224338, 77.58758101599165", help="Enter lat, lon as comma separated value")
+help_input = st.text_input(
+    label="Request Location",
+    value="13.064058531224338, 77.58758101599165",
+    help="Enter lat, lon as comma separated value",
+)
 help_loc = help_input.split(",")
 lat = float(help_loc[0].strip())
 lon = float(help_loc[1].strip())
 help_loc = (lat, lon)
 
-rider_count = 10
+rider_count = 20
 st.subheader(f"Your {rider_count} Closest riders are:")
+
 
 def get_distance(row):
     loc = (row["lat"], row["lon"])
@@ -49,11 +56,13 @@ def get_distance(row):
     # st.write(dist)
     return dist
 
+
 data = load_data()
 from copy import deepcopy
+
 df = deepcopy(data)
 # st.dataframe(df)
 df["dist"] = df.apply(get_distance, axis=1)
 # st.dataframe(df)
 sorted_df = df.sort_values(by="dist", ascending=True)
-st.table(sorted_df[:20])
+st.table(sorted_df[:rider_count])
